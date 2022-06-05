36f16dc32fc73b5d865ff8bddca6d0d32730fad2
superoverflow <me@superoverflow.com>
2022-06-05 22:42:12 +0100

    unmarshalling json

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"rsc.io/quote"
+	"io/ioutil"
+	"os"
 )
 
+type Trade struct {
+	TradeDate   string `json:"date"`
+	GroupId     string `json:"group_id"`
+	Leg         string `json:"leg"`
+	Currency    string `json:"currency"`
+	Amount      int    `json:"amount"`
+	Fee         int    `json:"fee"`
+	Exponent    int    `json:"exponent"`
+	Description string `json:"description"`
+	Direction   string `json:"direction"`
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	fmt.Println(quote.Go())
+	jsonFile, err := os.Open("sample.json")
+	check(err)
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var trades []Trade
+	json.Unmarshal(byteValue, &trades)
+	fmt.Println(trades)
 }
